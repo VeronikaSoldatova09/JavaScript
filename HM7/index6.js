431130b4f622ab1157079dc64241086a0c59b3f0
Veronika Soldatova <113257762+VeronikaSoldatova09@users.noreply.github.com>
2022-12-20 22:07:26 +0100

    Add files via upload

---
+++ HM7/index6.js
@@ -0,0 +1,5 @@
+const questionDegrees = 'Enter the value of degrees in Celsius';
+const Celsius = prompt(questionDegrees);
+const inFahrenheit = Celsius * 9/5 + 32;
+const result = `Degrees in Fahrenheit = ${inFahrenheit}`;
+alert(result)
