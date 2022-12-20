1916b70c4ac6aeff95db68a7dad8fe810d07cf65
Veronika Soldatova <113257762+VeronikaSoldatova09@users.noreply.github.com>
2022-12-20 21:13:15 +0100

    Add files via upload

---
+++ HM7/index3.js
@@ -0,0 +1,5 @@
+const side = 'Enter the length of side of the square';
+const answerSide = prompt(side);
+const perimetr = 4 * answerSide;
+const answerPerimetr = `Perimetr of the square will be = ${perimetr}cm`;
+alert(answerPerimetr)
